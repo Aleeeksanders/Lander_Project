800010f53c9f1d4e0d13ed19b2bc4ff75375145a
Aleeeksanders <alexbmelita@gmail.com>
2024-11-07 18:05:57 -0300

    Falta API_KEY

--- tests/test_openai.py
+++ tests/test_openai.py
@@ -1,8 +1,6 @@
 import openai
 
 # Reemplaza 'tu_clave_api_aquí' con tu clave API real
-openai.api_key = "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"
-
 response = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",  # Cambia a 'gpt-4' si tienes acceso
     messages=[
